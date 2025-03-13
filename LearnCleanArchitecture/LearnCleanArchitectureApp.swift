1f06134bfb07fa18f01aab85f4d73fadec9ed6cd
chxhyxn <chxhyxn@gmail.com>
2025-03-13 17:52:36 +0900

    Initial Commit

---
+++ LearnCleanArchitecture/LearnCleanArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnCleanArchitectureApp.swift
+//  LearnCleanArchitecture
+//
+//  Created by SeanCho on 3/13/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnCleanArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
